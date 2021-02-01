926f9cb6517b85799d95aa2398cf0c5046df1d01
Dongwoo Yang <ckck803@gmail.com>
2021-02-02 02:16:48 +0900

    feat : OAuth2를 이용한 로그인
    - localhost:8080/restricted에 접근하게 되면 Google로그인페이지로 이동하게 된다

---
+++ security-oauth2/src/main/java/com/example/sampleoauth2/SampleOAuth2Application.java
@@ -0,0 +1,11 @@
+package com.example.sampleoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOAuth2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleOAuth2Application.class, args);
+    }
+}
